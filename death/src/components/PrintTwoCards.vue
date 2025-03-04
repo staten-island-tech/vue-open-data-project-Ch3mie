e255b9c77503a147245f2c35e1036cb03a1da87f
Ch3mie <ryanc668@nycstudents.net>
2025-03-04 12:31:50 -0500

    e

---
+++ death/src/components/PrintTwoCards.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+let data1 = null
+let data2 = null
+function ChooseTwoCards() {
+  //get the data, assign some shit to each data, then log the two data as like data 1 and data 2 then print it
+}
+</script>
+
+<style scoped></style>
--- death/src/views/HomeView.vue
+++ death/src/views/HomeView.vue
@@ -1,9 +1,31 @@
+<template>
+  <div>
+    <h2>Data</h2>
+    <div v-for="(item, index) in SetData" :key="index">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+
+const SetData = ref([]) // Should be an array, not a string
+
+async function getData() {
+  try {
+    let res = await fetch(`https://data.cityofnewyork.us/resource/25th-nujf.json`)
+    let data = await res.json()
+    SetData.value = data // Fix variable name
+    console.log(data)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+onMounted(() => {
+  getData()
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
