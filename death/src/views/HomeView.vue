3e0509335a2a3a08ac88048fcc279be4e57cb705
Ch3mie <ryanc668@nycstudents.net>
2025-03-06 11:24:45 -0500

    Update HomeView.vue

--- death/src/views/HomeView.vue
+++ death/src/views/HomeView.vue
@@ -1,26 +1,23 @@
 <template>
   <div>
     <h2>Data</h2>
-    <div v-for="(item, index) in SetData" :key="index">
-      {{ item.brth_yr }}
+    <div v-for="datas in data" :key=".id">
+      {{ item }}
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-
-const SetData = ref([])
-
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const Data = ref('')
 async function getData() {
-  try {
-    let res = await fetch(`https://data.cityofnewyork.us/resource/25th-nujf.json`)
-    let data = await res.json()
-    SetData.value = data
-    console.log(data)
-  } catch (error) {
-    console.error('Error fetching data:', error)
-  }
+  let res = await fetch(`https://data.cityofnewyork.us/resource/jb7j-dtam.json`)
+  let data = await res.json()
+  satData.value = data
+  console.log(data)
+  return data
 }
 
 onMounted(() => {
