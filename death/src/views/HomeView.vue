e529930cc1da5f5ff967c069d2f36800a5341d18
Ch3mie <ryanc668@nycstudents.net>
2025-03-10 12:20:01 -0400

    e

--- death/src/views/HomeView.vue
+++ death/src/views/HomeView.vue
@@ -2,7 +2,10 @@
   <div>
     <h2>Data</h2>
     <div v-for="(item, index) in setData" :key="index">
-      {{ item }}
+      <p>Race: {{ item.race_ethnicity }}</p>
+      <p>Sex: {{ item.sex }}</p>
+      <p>Cause of Death:{{ item.leading_cause }}</p>
+      <p>____________________________</p>
     </div>
   </div>
 </template>
@@ -10,13 +13,13 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-const setData = ref([]) // Should be an array, not a string
+const setData = ref([])
 
 async function getData() {
   try {
     let res = await fetch(`https://data.cityofnewyork.us/resource/jb7j-dtam.json`)
     let data = await res.json()
-    setData.value = data // Fix variable name
+    setData.value = data
     console.log(data)
   } catch (error) {
     console.error('Error fetching data:', error)
